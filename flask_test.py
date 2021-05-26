8af4606508b002d1e95658dac6a33e2496f0fdf8
carcher54321 <79350263+carcher54321@users.noreply.github.com>
2021-05-26 14:50:20 -0400

    Update flask_test.py

--- flask_test.py
+++ flask_test.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('', methods=['GET'])
+@app.route('/', methods=['GET'])
 def test():
     return jsonify({'message': 'Test Successful'})
 
